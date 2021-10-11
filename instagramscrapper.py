2690d655e91c3811bdecd180a9c35a3796f0a8e3
JAGGANRAJ <jaggan27@gmail.com>
2021-10-11 09:00:54 +0530

    Revert "Update instagramscrapper.py"
    This reverts commit 0ff13e82b6145f55dbbb387ce7531f7c39ab48ef.

--- instagramscrapper.py
+++ instagramscrapper.py
@@ -1,5 +1,4 @@
 from instagram_scraper import InstagramScraper
-scraper = InstagramScraper(usernames = [''])
 scraper = InstagramScraper(usernames = ['']) #enter Insta ID in single qoutes
 scraper.authenticate_as_guest()
 scraper.scrape()
